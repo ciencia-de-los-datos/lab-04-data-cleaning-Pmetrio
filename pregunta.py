3409dcce403410c6d8c87d76831f2d97e28e7536
Pmetrio <160280923+Pmetrio@users.noreply.github.com>
2024-04-09 14:57:37 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,7 +1,6 @@
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
-
 Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
